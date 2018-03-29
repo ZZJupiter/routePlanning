864e111d51ec2e3728ad95c39b0e8d671e81f124
zhangzhong <zlyxzq@163.com>
2018-03-29 21:37:34 +0800

    1.增加二维码功能

--- web/src/main/java/com/hanma56/cloud/route/web/controller/line/LineRouteController.java
+++ web/src/main/java/com/hanma56/cloud/route/web/controller/line/LineRouteController.java
@@ -1,14 +1,18 @@
 package com.hanma56.cloud.route.web.controller.line;
 
-
+import com.google.zxing.WriterException;
 import com.hanma56.cloud.route.convert.PointConvert;
+import com.hanma56.cloud.route.integration.baidu.ShortUrlService;
 import com.hanma56.cloud.route.model.LineRoute;
 import com.hanma56.cloud.route.service.LineRouteService;
+import com.hanma56.cloud.route.util.MatrixToImageWriter;
 import com.hanma56.cloud.route.web.controller.line.request.DeleteLinRouteForm;
+import com.hanma56.cloud.route.web.controller.line.request.IdForm;
 import com.hanma56.cloud.route.web.controller.line.request.QueryLineRouteForm;
 import com.hanma56.cloud.route.web.controller.line.request.SaveLinRouteForm;
 import com.hanma56.cloud.route.web.result.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +21,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -31,43 +37,52 @@ import java.util.List;
 @RequestMapping("/lineRoute")
 public class LineRouteController {
 
+    @Value("${app.host}")
+    private String           host;
+
     @Autowired
     private LineRouteService lineRouteService;
 
-
     @ResponseBody
     @RequestMapping(value = "/save")
-    public ResultVO save(@RequestBody SaveLinRouteForm form){
+    public ResultVO save(@RequestBody SaveLinRouteForm form) {
 
-       List<LineRoute>  lineRoutes = PointConvert.convert(form.getPoints(),form.getLineId(),1l);
+        List<LineRoute> lineRoutes = PointConvert.convert(form.getPoints(), form.getLineId(), 1l);
 
-       lineRouteService.batchSaveLinePoint(lineRoutes);
+        lineRouteService.batchSaveLinePoint(lineRoutes);
 
-       return ResultVO.success();
+        return ResultVO.success();
     }
 
     @ResponseBody
     @RequestMapping(value = "/get")
-    public ResultVO<List<LineRoute>> getLineRouteByLineId(@RequestBody QueryLineRouteForm form){
+    public ResultVO<List<LineRoute>> getLineRouteByLineId(@RequestBody QueryLineRouteForm form) {
         List<LineRoute> lineRouteList = lineRouteService.queryByLineId(form.getLineId());
         return ResultVO.success(lineRouteList);
     }
 
     @ResponseBody
     @RequestMapping(value = "/delete")
-    public ResultVO deleteById(@RequestBody DeleteLinRouteForm form){
+    public ResultVO deleteById(@RequestBody DeleteLinRouteForm form) {
         lineRouteService.deleteById(form.getId());
         return ResultVO.success();
     }
 
-    @RequestMapping(value = "/map")
-    public ModelAndView showRouteMap(Long lineId, Model model){
+    @RequestMapping(value = "/route")
+    public ModelAndView showRouteMap(Long lineId, Model model) {
         List<LineRoute> lineRouteList = lineRouteService.queryByLineId(lineId);
-        model.addAttribute("lineRouteList",lineRouteList);
+        model.addAttribute("lineRouteList", lineRouteList);
         return new ModelAndView("line/route");
     }
 
+    @RequestMapping(value = { "/qrcode" })
+    public void getQRCodeBase64(Long lineId, HttpServletResponse response) throws IOException,
+                                                                           WriterException {
 
+        String sourceUrl = host + "/lineRoute/route?lineId=" + lineId;
+        //        String shortUrl = ShortUrlService.generateShortUrl(sourceUrl);
+        MatrixToImageWriter.createRqCode(sourceUrl, Integer.valueOf(100), Integer.valueOf(100),
+            response.getOutputStream());
+    }
 
 }
-
